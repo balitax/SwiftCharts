d4b0690b0d71b77ee829ac39da07f2e65f04edc1
Agus Cahyono <cahyo.mamen@gmail.com>
2026-01-05 11:45:34 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.6
+
 import PackageDescription
 
 let package = Package(
      name: "SwiftCharts",
      platforms: [
-         .iOS(.v9)
+         .iOS(.v13)
      ],
      products: [
          .library(name: "SwiftCharts", type: .dynamic, targets: ["SwiftCharts"])
